8ab48cf7bc5e2db6bfed12c5ea049f000df5d293
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-05-10 02:05:55 +0300

    🪻 style: Posts styling

--- frontend/src/components/Posts/PostItem.jsx
+++ frontend/src/components/Posts/PostItem.jsx
@@ -9,7 +9,7 @@ const PostItem = ({post}) => {
       <div className="card-body">
         <h2 className="card-title">
           Shoes!
-          <div className="badge badge-secondary">NEW</div>
+          <div className="badge badge-primary">NEW</div>
         </h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
         <div className="card-actions justify-end">
