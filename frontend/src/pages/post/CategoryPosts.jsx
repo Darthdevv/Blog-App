becfbca5d3ac71645812c7f672ee2e4f88e7fcd9
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-05-16 00:49:45 +0300

    🖋️feat: authorPosts and categoryPostse

--- frontend/src/pages/post/CategoryPosts.jsx
+++ frontend/src/pages/post/CategoryPosts.jsx
@@ -1,9 +1,18 @@
-
+import React, { useState } from "react";
+import PostItem from "../../components/Posts/PostItem";
+import { postData } from "../../utilities/data";
 
 const CategoryPosts = () => {
+  const [posts, setPosts] = useState(postData);
   return (
-    <div>CategoryPosts</div>
-  )
-}
+    <div className=" max-w-[1100px] mx-auto py-20 flex items-start justify-start max-lg:items-center max-lg:justify-center flex-wrap gap-y-8">
+      {posts.length > 0 ? (
+        posts.map((post) => <PostItem key={post.id} post={post} />)
+      ) : (
+        <p>No Posts Found 🤦🏻‍♂️</p>
+      )}
+    </div>
+  );
+};
 
-export default CategoryPosts
+export default CategoryPosts;
