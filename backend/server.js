4da949f077edfb898cc28dcceb72c5bd5ffe188b
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-05-21 03:27:17 +0300

    chore: connecting to Mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import connectToMongoDB from './database/connectToMongoDB.js';
 import dotenv from 'dotenv';
+import e from 'express';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
