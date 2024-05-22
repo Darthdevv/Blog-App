f7a83e9d57a7e57138bad61e1c529ff0ce159389
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-05-22 03:54:51 +0300

    chore: creating user routes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,23 @@
-export const getUsers = (req, res) => {
-  res.status(200).json('these are all the users');
+export const registerUser = (req, res) => {
+  res.status(200).json('the user has been registered');
 }
+
+export const loginUser = (req, res) => {
+  res.status(200).json("the user has logged in successfully");
+};
+
+export const getUsers = (req, res) => {
+  res.status(200).json("these are the users");
+};
+
+export const getUser = (req, res) => {
+  res.status(200).json("this is the user");
+};
+
+export const changeAvatar = (req, res) => {
+  res.status(200).json("avatar has been changed");
+};
+
+export const editUser = (req, res) => {
+  res.status(200).json("the user has been updated");
+};
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { getUsers } from "../controllers/user.controller.js";
+import { getUser, getUsers, editUser, changeAvatar, registerUser, loginUser } from "../controllers/user.controller.js";
 const router = Router();
 
 router.route('/').get(getUsers);
+router.route("/:id").get(getUser);
+router.route("/login").post(loginUser);
+router.route("/register").post(registerUser);
+router.route("/change-avatar").post(changeAvatar);
+router.route("/edit-user").post(editUser);
 
 
 export default router;
