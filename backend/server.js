be8b86bc95310a55de5b4b9f0cd91da8f83a8702
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-05-26 07:27:36 +0300

    🪻 feat: changing avatar functionality

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 import connectToMongoDB from './database/connectToMongoDB.js';
 import dotenv from 'dotenv';
+import upload from 'express-fileupload';
 import userRoutes from './routes/user.routes.js';
 import { errorHandler, notFound } from './middlewares/error.middleware.js';
 
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(upload());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
