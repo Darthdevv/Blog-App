a3de0bb8229ded0e5d1bbab50b054d4400490568
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-05-26 03:18:04 +0300

    🪻 feat: creating user model

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,28 @@
-export const registerUser = (req, res) => {
-  res.status(200).json('the user has been registered');
+import User from "../models/user.model.js";
+import appError from '../utilities/appError.js'
+
+export const registerUser = async (req, res, next) => {
+  try {
+    const { name, email, password, paassword2 } = req.body;
+    if (!name || !email || !password) {
+      return next( new appError('Fill in all fields.', 422))
+    }
+
+    const newEmail = email.toLowerCase();
+
+    const emailExists = await User.findOne({ email: newEmail });
+
+    if (emailExists) {
+      return next(new appError('Email already exists', 422));
+    }
+
+    if ((password.trim()).length < 6) {
+      return next(new appError('Password must be at least 6 characters'), 422);
+    }
+    
+  } catch (error) {
+    return next(new appError('User Registration failed', 422));
+  }
 }
 
 export const loginUser = (req, res) => {
---
+++ backend/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: 'string',
+    required: true
+  },
+  email: {
+    type: 'string',
+    required: true
+  },
+  password: {
+    type: 'string',
+    required: true
+  },
+  avatar: {
+    type: 'string'
+  },
+  posts: {
+    type: 'number',
+    default: 0
+  },
+})
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
